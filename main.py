04c7e9f9d302a05576110945759d26876e7719c7
Freddy Rosa <freddy.rosa16@gmail.com>
2025-06-22 15:20:13 -0400

    Refactor main and generate_content functions to enhance argument handling and improve verbose output

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    verbose = "--verbose" in sys.argv
 
     if not args:
         print("AI Code Assistant")
@@ -19,17 +20,20 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    user_prompt = " ".join(args)
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-    generate_content(client, messages)
+    generate_content(client, messages, verbose, user_prompt)
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose, user_prompt):
     response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+
     
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    else:
+        print("Response:")
+        print(response.text)
 
 if __name__ == "__main__":
     main()

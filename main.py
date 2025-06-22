469a0f901499a0eb91d653091bf529100c21fd3b
Freddy Rosa <freddy.rosa16@gmail.com>
2025-06-22 13:58:38 -0400

    Refactor main function to improve message handling and separate content generation logic

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
-    
+
     args = sys.argv[1:]
 
     if not args:
@@ -17,7 +18,13 @@ def main():
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=user_prompt)
+
+    user_prompt = " ".join(args)
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)

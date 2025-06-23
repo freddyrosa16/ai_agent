1255df9c78c1d25848be32fe76e977a66198cf71
Freddy Rosa <freddy.rosa16@gmail.com>
2025-06-23 11:55:50 -0400

    Add get_file_content function to validate file access and existence

---
+++ functions/get_file_content.py
@@ -0,0 +1,11 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    absolute_working_directory = os.path.abspath(working_directory)
+    absolute_file_path = os.path.abspath(file_path)
+
+    if not absolute_file_path.startswith(absolute_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(absolute_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'

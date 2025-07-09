a04e3ccb181df161c46df3d6442fa6c6c2c884a2
Freddy Rosa <freddy.rosa16@gmail.com>
2025-07-09 18:23:26 -0400

    Refactor main.py to utilize available_functions from call_function.py for improved modularity

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_files_content
-from functions.run_python import schema_run_python_file
-from functions.write_file import schema_write_file
+from call_function import available_functions 
 
 
 def main():
@@ -31,8 +28,6 @@ def main():
     generate_content(client, messages, verbose, user_prompt)
 
 def generate_content(client, messages, verbose, user_prompt):
-    available_functions = types.Tool(
-    function_declarations=[schema_get_files_info, schema_get_files_content, schema_run_python_file, schema_write_file,])
     response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(
     tools=[available_functions], system_instruction=system_prompt
 ),)
@@ -42,12 +37,13 @@ def generate_content(client, messages, verbose, user_prompt):
         print(f"User prompt: {user_prompt}")
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    else:
-        if response.function_calls:
-            for function_call_part in response.function_calls:
-                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        print("Response:")
-        print(response.text)
+
+    if not response.function_calls:
+        return response.text
+    
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    
 
 if __name__ == "__main__":
     main()

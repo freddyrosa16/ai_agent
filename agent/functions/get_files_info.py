55261d0d8a61439d088bb315fc839f1c9661711f
Freddy Rosa <freddy.rosa16@gmail.com>
2025-10-18 08:02:59 -0400

    Add get_files_info function to retrieve file details in a specified directory

---
+++ agent/functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_target = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not abs_target.startswith(abs_working_directory):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not os.path.isdir(abs_target):
+        f'Error: "{directory}" is not a directory'
+
+    try:    
+        lines = []
+        for filename in os.listdir(abs_target):
+            path = os.path.join(abs_target, filename)
+            size = os.path.getsize(path)
+            is_file = os.path.isfile(path)
+            lines.append(f"- {filename}: file_size={size} bytes, is_dir={is_file}")
+        return "\n".join(lines)
+    except Exception as err:
+        return str(f"{err}")

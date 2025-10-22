0c0b06b19719826be925b8d843bc3f2eec3c47ac
Freddy Rosa <freddy.rosa16@gmail.com>
2025-10-22 16:39:18 -0400

    Refactor main function for improved prompt handling and verbosity output

--- agent/main.py
+++ agent/main.py
@@ -2,25 +2,63 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from functions.get_files_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_files_content import schema_write_file
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    verbose = "--verbose"
-    user_prompt = sys.argv
-    if len(user_prompt) < 1:
-        print("Error: Please enter a prompt.")
+    is_verbose = "--verbose" in sys.argv
+    model_name = 'gemini-2.0-flash-001'
+    system_prompt = """
+    You are a helpful AI coding agent.
+    
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
+    
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
-    response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=user_prompt
+    args = [a for a in sys.argv[1:] if a != "--verbose"]
+    if not args:
+        print("Error: Please enter a prompt.")
+        return
+    prompt = " ".join(args)
+    
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ]
 )
-    if verbose in user_prompt:
-        print(f"User prompt: {user_prompt}")
+    response = client.models.generate_content(
+    model=model_name,
+    contents=prompt,
+    config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+    )
+)
+    if is_verbose:
+        print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
-        print(response.text)
+        if len(response.function_calls) > 0:
+            for function_call_part in response.function_calls:
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(response.text)
 
 if __name__ == "__main__":
     main()
